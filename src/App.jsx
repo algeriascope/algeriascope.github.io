9e514432159f02cf5823abbb3de0e2df98979292
aegon78 <aegonguru@gmail.com>
2026-09-15 22:58:12 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Exchange from './components/Exchange';
+
 const app = () => {
   return (
     <>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,27 @@
 import react from 'react';
-import styles from './Navbar.module.css'
+import styles from './Navbar.module.css';
 import { Link } from 'react-router-dom';
+import { MdCurrencyExchange, MdOutlineSettings } from "react-icons/md";
+import { LuLanguages } from "react-icons/lu";
 
-const navbar = () => {
 
-  return (
-    <ul styles>
-    
-      <li>
-        <Link to="/">algeriascope</Link>
-      </li>
-      <li>
-        <Link to="/">home</Link>
-      </li>
-      <li>
-        <Link to="/exchange">exchange</Link>
-      </li>
-    </ul>
-  );
-};
+const navbar = () => (
+    <nav className={styles.navbarContainer}>
+      <div className={styles.logo}>
+        <Link to="/">AlgeriaScope</Link>
+      </div>
+      <ul className={styles.linksContainer}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/exchange"> <MdCurrencyExchange />Exchange</Link>
+        </li>
+        <li>
+          <Link to="/Settings"><LuLanguages /></Link>
+        </li>
+      </ul>
+    </nav>
+);
 
 export default navbar;
