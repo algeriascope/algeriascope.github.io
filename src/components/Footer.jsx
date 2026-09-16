ae41056f05ca0ffff81b8becbbdd8a99175c511c
aegon78 <aegonguru@gmail.com>
2026-09-16 18:55:10 +0100

    fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.css';
 const Footer = () => {
   return (
     <footer>
-      <ul>
+      <ul className={styles.footerUl}>
         <li>
           <Link to="/rates">Rates</Link>
         </li>
@@ -12,9 +12,10 @@ const Footer = () => {
           <Link to="/about">About</Link>
         </li>
         <li>
-
+          <Link to="/donate">Donate</Link>
         </li>
       </ul>
+      <p className={styles.copyright}>© <span>{new Date().getFullYear()}</span> </p>
     </footer>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
 import styles from './Navbar.module.css';
 import { Link } from 'react-router-dom';
-import { MdCurrencyExchange, MdOutlineSettings } from "react-icons/md";
-import { LuLanguages } from "react-icons/lu";
-
+import { MdOutlineDarkMode, MdOutlineLightMode } from 'react-icons/md';
+import { LuLanguages } from 'react-icons/lu';
 
 const Navbar = () => (
-    <nav className={styles.navbarContainer}>
-      <div className={styles.logo}>
-        <Link to="/">AlgeriaScope</Link>
-      </div>
-      <ul className={styles.linksContainer}>
-        
-        <li>
-          <Link to="/rates"> Rates</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <button>
-          <LuLanguages />
-        </button>
-      </ul>
-    </nav>
+  <nav className={styles.navbarContainer}>
+    <div className={styles.logo}>
+      <Link to="/">AlgeriaScope</Link>
+    </div>
+    <ul className={styles.linksContainer}>
+      <li>
+        <Link to="/rates"> Rates</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+
+      <button className={styles.utilityBtn}>
+        <MdOutlineDarkMode
+          className={`${styles.dModeIcon}, ${styles.themeIcon}`}
+        />
+        <MdOutlineLightMode
+          className={`${styles.lModeIcon}, ${styles.themeIcon}`}
+        />
+      </button>
+
+      <button className={styles.utilityBtn}>
+        <LuLanguages className={styles.languageIcon} />
+      </button>
+    </ul>
+  </nav>
 );
 
 export default Navbar;
