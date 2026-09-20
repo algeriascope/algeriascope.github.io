096789a5c154e0b858a7f9a3c109a91ca4dc07cf
aegon78 <aegonguru@gmail.com>
2026-09-20 01:48:13 +0100

    react-svg for maps icons/ui fixes

--- src/pages/Postcodes.jsx
+++ src/pages/Postcodes.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { ReactSVG } from 'react-svg';
 import styles from './Postcodes.module.css';
 import data from '../data/algeria_data.json';
 import { Link } from 'react-router-dom';
+import { FaCity, FaBuildingColumns } from "react-icons/fa6";
 
 const wilayaMaps = import.meta.glob('../assets/maps/*.svg', {
   eager: true,
@@ -16,13 +18,22 @@ const Postcodes = () => {
           const mapSvg = wilayaMaps[`../assets/maps/${wilaya.wilaya_code}.svg`];
           return (
             <Link
-              to={`/postcodes/${wilaya.wilaya_name}`}
+              key={wilaya.wilaya_name}
+              to={`/postcodes/${wilaya.wilaya_name.toLocaleLowerCase()}`}
               className={styles.wilayaCard}
             >
-              <img src={mapSvg} alt={wilaya.wilaya_name} />
+              <ReactSVG className={styles.reactSvg} src={mapSvg}/>
               <div className={styles.codeAndName}>
                 <span className={styles.code}>{wilaya.wilaya_code} </span>
                 <span className={styles.name}>{wilaya.wilaya_name}</span>
+              </div>
+              <div className={styles.statsContainer}>
+                <span className={styles.communes}>
+                  <FaCity className={styles.statIcon}/>0 Communes
+                </span>
+                <span className={styles.postOffices}>
+                  <FaBuildingColumns className={styles.statIcon}/> {wilaya.postcodes.length} Post Offices
+                </span>                
               </div>
             </Link>
           );
